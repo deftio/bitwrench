507da92764fa44a34df7ab668264e79cfd82d5b8
deftio <deftio@deftio.com>
2026-03-08 00:03:12 -0800

    v2.0.14 release

--- src/version.js
+++ src/version.js
@@ -12,5 +12,5 @@ export const VERSION_INFO = {
   homepage: 'https://deftio.github.com/bitwrench/pages',
   repository: 'git+https://github.com/deftio/bitwrench.git',
   author: 'manu a. chatterjee <deftio@deftio.com> (https://deftio.com/)',
-  buildDate: '2026-03-08T07:51:17.655Z'
+  buildDate: '2026-03-08T08:03:00.721Z'
 };
