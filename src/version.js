b71ecfc4e4210f7c451382d2d773d60f51229822
deftio <deftio@deftio.com>
2026-03-10 02:11:18 -0700

    start v2.0.16: phase2-component-handles

--- src/version.js
+++ src/version.js
@@ -3,14 +3,14 @@
  * DO NOT EDIT DIRECTLY - Use npm run generate-version
  */
 
-export const VERSION = '2.0.15';
+export const VERSION = '2.0.16';
 export const VERSION_INFO = {
-  version: '2.0.15',
+  version: '2.0.16',
   name: 'bitwrench',
   description: 'A library for javascript UI functions.',
   license: 'BSD-2-Clause',
   homepage: 'https://deftio.github.com/bitwrench/pages',
   repository: 'git+https://github.com/deftio/bitwrench.git',
   author: 'manu a. chatterjee <deftio@deftio.com> (https://deftio.com/)',
-  buildDate: '2026-03-10T09:05:58.828Z'
+  buildDate: '2026-03-10T09:11:18.719Z'
 };
