2aa269c232e3974688ae0df12cc3cc59913c5899
deftio <deftio@deftio.com>
2026-03-23 22:24:49 -0700

    v2.0.21 routing and docs update

--- src/version.js
+++ src/version.js
@@ -12,5 +12,5 @@ export const VERSION_INFO = {
   homepage: 'https://deftio.github.com/bitwrench/pages',
   repository: 'git+https://github.com/deftio/bitwrench.git',
   author: 'manu a. chatterjee <deftio@deftio.com> (https://deftio.com/)',
-  buildDate: '2026-03-24T05:00:30.853Z'
+  buildDate: '2026-03-24T05:18:13.573Z'
 };
