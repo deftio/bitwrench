8fa139520c16c16c6002caa7ccf85b5d345bd19d
deftio <deftio@deftio.com>
2026-03-23 22:35:38 -0700

    start v2.0.22: readme-fix

--- src/version.js
+++ src/version.js
@@ -3,14 +3,14 @@
  * DO NOT EDIT DIRECTLY - Use npm run generate-version
  */
 
-export const VERSION = '2.0.21';
+export const VERSION = '2.0.22';
 export const VERSION_INFO = {
-  version: '2.0.21',
+  version: '2.0.22',
   name: 'bitwrench',
   description: 'A library for javascript UI functions.',
   license: 'BSD-2-Clause',
   homepage: 'https://deftio.github.com/bitwrench/pages',
   repository: 'git+https://github.com/deftio/bitwrench.git',
   author: 'manu a. chatterjee <deftio@deftio.com> (https://deftio.com/)',
-  buildDate: '2026-03-24T05:27:30.802Z'
+  buildDate: '2026-03-24T05:35:38.649Z'
 };
